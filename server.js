63ed1eb0ddb515add6748f1d456cac8753efdbc6
gnragazzi <gnragazzi@gmail.com>
2021-04-14 16:10:20 -0300

    First Draft

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var cors = require('cors');
+var express = require('express')
+var cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
+const multer = require('multer')
+var app = express()
+var upload = multer({ dest: 'uploads/' })
 
-var app = express();
-
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const { originalname: name, mimetype: type, size } = req.file
+  next()
+  res.json({ name, type, size })
+})
 
-
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
